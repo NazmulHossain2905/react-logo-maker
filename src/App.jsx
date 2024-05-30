d446e8dd95623861893bff2f89242c63bf0f47fb
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-05-30 18:45:02 +0600

    setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return;
+  <div class="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
+    <h1>Hello World</h1>
+  </div>;
 };
 
 export default App;
