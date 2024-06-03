e218ab61a988cf3c0d9e058544011d362ee52fca
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-06-03 22:13:16 +0600

    footer section added on about page and more logo added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,16 +24,18 @@ const About = () => {
 
   return (
     <div className="body-height overflow-auto">
-      <div className="flex h-[450px] flex-col items-center justify-center gap-5 bg-red-500 bg-[url('/about_bg.png')] bg-cover bg-no-repeat">
-        <h1 className="rounded-full border-b-4 border-slate-300 bg-black bg-opacity-80 p-2 px-8 text-2xl font-bold uppercase text-blue-400 sm:text-4xl md:text-7xl">
-          Welcome to LogoMaker
-        </h1>
-        <p className="rounded-full border-b-4 border-gray-300 bg-white bg-opacity-30 p-1.5 px-6 text-lg italic text-black sm:text-2xl md:p-3 md:text-5xl">
-          Create your own logo with{" "}
-          <span className="rounded-full border-b-4 border-blue-500 bg-black px-3 font-medium not-italic text-white">
-            LogoMaker
-          </span>
-        </p>
+      <div className="h-[450px] bg-[url('/about_bg.png')] bg-cover bg-no-repeat">
+        <div className="flex h-full flex-col items-center justify-center gap-5 bg-black bg-opacity-15">
+          <h1 className="rounded-full border-b-4 border-slate-300 bg-black bg-opacity-80 p-2 px-8 text-2xl font-bold uppercase text-blue-400 sm:text-4xl md:text-7xl">
+            Welcome to LogoMaker
+          </h1>
+          <p className="rounded-full border-b-4 border-gray-300 bg-white bg-opacity-30 p-1.5 px-6 text-lg italic text-black sm:text-2xl md:p-3 md:text-5xl">
+            Create your own logo with{" "}
+            <span className="rounded-full border-b-4 border-blue-500 bg-black px-3 font-medium not-italic text-white">
+              LogoMaker
+            </span>
+          </p>
+        </div>
       </div>
 
       <div className="mx-auto mt-20 max-w-screen-xl px-3">
@@ -62,10 +64,10 @@ const About = () => {
         </h2>
 
         <div className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-6">
-          {Array(7)
+          {Array(12)
             .fill(0)
             .map((_, i) => (
-              <div className="group aspect-square cursor-pointer border border-dashed bg-transparent transition hover:border-transparent hover:bg-slate-200 ">
+              <div className="group aspect-square cursor-pointer border border-dashed">
                 <img
                   className="size-full transition group-hover:scale-110"
                   src={`/logo-${i + 1}.png`}
@@ -92,6 +94,18 @@ const About = () => {
             <li>Download your logo in high resolution, ready for use.</li>
           </ol>
         </div>
+      </div>
+
+      <div className="bg-gray-800 p-12 text-center text-lg text-gray-300">
+        Developed by <strong>Nazmul Hossain</strong>. All Rights Reserved 2024.
+        •{" "}
+        <a
+          className="text-blue-500 hover:text-blue-600 hover:underline"
+          href="https://github.com/NazmulHossain2905/react-logo-maker"
+          target="_blank"
+        >
+          GitHub Repository
+        </a>
       </div>
     </div>
   );
