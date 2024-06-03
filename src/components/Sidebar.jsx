0f5587a1a09874ecc1df77c677b27fc62cee6598
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-06-03 14:27:15 +0600

    developed by added on sidebar component at the bottom

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,10 +28,11 @@ const Sidebar = ({ handleOnChangeBtn }) => {
   const [activeBtnIndex, setActiveBtnIndex] = useState(0);
 
   return (
-    <div className="m-3 mt-5 flex flex-col gap-4">
-      {buttons.map((btn, index) => (
-        <button
-          className={`flex items-center justify-start gap-4
+    <div className="flex h-full flex-col justify-between">
+      <div className="m-3 mt-5 flex flex-col gap-4">
+        {buttons.map((btn, index) => (
+          <button
+            className={`flex items-center justify-start gap-4
            rounded-md border p-4 px-5 font-semibold transition-colors
           hover:bg-blue-500 hover:text-white
               ${
@@ -39,16 +40,32 @@ const Sidebar = ({ handleOnChangeBtn }) => {
                   ? "bg-blue-500 text-white"
                   : "bg-transparent text-blue-500"
               }`}
-          key={btn.id}
-          onClick={() => {
-            setActiveBtnIndex(index);
-            handleOnChangeBtn(index);
-          }}
-        >
-          <btn.icon className="size-5" />
-          {btn.title}
-        </button>
-      ))}
+            key={btn.id}
+            onClick={() => {
+              setActiveBtnIndex(index);
+              handleOnChangeBtn(index);
+            }}
+          >
+            <btn.icon className="size-5" />
+            {btn.title}
+          </button>
+        ))}
+      </div>
+
+      <div className="border-t p-3">
+        <p className="text-center">
+          Developed by{" "}
+          <a
+            className="cursor-pointer font-medium text-blue-500 hover:underline"
+            target="_blank"
+            href="https://github.com/NazmulHossain2905/"
+          >
+            Nazmul Hossain.
+          </a>
+          <br />
+          All Reserved 2024
+        </p>
+      </div>
     </div>
   );
 };
